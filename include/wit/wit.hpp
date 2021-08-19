d151e059d3a1006950dcc857547586c068fb529f
Jacob Gamble <gamblejr@prototypeind.com>
2021-08-18 20:46:57 -0500

    Adding the ability to write one constructor and have the others generated

--- include/wit/wit.hpp
+++ include/wit/wit.hpp
@@ -1,9 +1,84 @@
 #ifndef WIT_WIT_HPP
 #define WIT_WIT_HPP
+#include <memory>
+#include <type_traits>
+#include <tuple>
+#include <functional>
 
 namespace wit
 {
 
+    template<typename... Ts>
+    struct get_last
+    {
+        using tup = std::tuple<Ts...>;
+        using type = typename std::tuple_element<sizeof...(Ts) - 1, tup>::type;
+    };
+
+    template<>
+    struct get_last<>
+    {
+        using type = void;
+    };
+
+    template<typename... T>
+    using get_last_t = typename get_last<T...>::type;
+
+    template<int index>
+    struct placeholder {};
+}
+namespace std
+{
+    template<int index>
+    struct is_placeholder<wit::placeholder<index>>
+        : integral_constant<int, index> {};
+}
+namespace wit
+{
+    // TODO: Perfect forwarding
+    template<typename host_t, typename... T>
+    host_t create(T... t)
+    {
+        return host_t(t...);
+    }
+
+    template<typename host_t, typename int_t, int_t... ints, typename... T>
+    auto create_helper(std::integer_sequence<int_t, ints...>, T... ts)
+    {
+        auto cimpl = create<host_t, std::allocator_arg_t,
+             typename std::tuple_element<(ints+1)%sizeof...(ints),std::tuple<T...>>::type...>;
+        auto x = std::bind(cimpl, std::allocator_arg,
+                placeholder<((ints + 1) % sizeof...(ints))+1>{}...);
+
+        return x(ts...);
+    }
+
+    template<typename host_t>
+    struct wit : host_t
+    {
+        using allocator_type = typename host_t::allocator_type;
+
+        template<typename... T>
+        wit(std::allocator_arg_t, allocator_type a, T&&... t) :
+            host_t(std::allocator_arg, a, std::forward<T>(t)...) {}
+
+        template<typename... T,
+            typename V=std::enable_if_t<
+                std::is_same<get_last_t<T...>, allocator_type>::value
+            >,
+            typename U = V
+        >
+        wit(T&&... t)
+            : host_t(create_helper<host_t>(std::make_index_sequence<sizeof...(T)>{}, t...)) {}
+
+        template<typename... T,
+            typename=std::enable_if_t<
+                !std::is_same<get_last_t<T...>, allocator_type>::value
+            >
+        >
+        wit(T&&... t) :
+            host_t(std::allocator_arg, allocator_type(), std::forward<T>(t)...) {}
+    };
 }
 
 #endif
--- test/wit.cpp
+++ test/wit.cpp
@@ -1,7 +1,31 @@
 #include "wit/wit.hpp"
 #include <cassert>
+#include <memory>
+
+template<typename Alloc = std::allocator<char>>
+struct alloc_aware_impl
+{
+    using allocator_type = std::allocator<char>;
+    using string_t = std::basic_string<char, std::char_traits<char>, allocator_type>;
+    string_t str;
+
+    alloc_aware_impl(std::allocator_arg_t, Alloc const& a, const char* str = "") : str(str, a)
+    {
+    }
+};
+
+using alloc_aware = wit::wit<alloc_aware_impl<>>;
 
 int main()
 {
+    alloc_aware x;
+    assert(x.str == "");
+
+    alloc_aware y = "test";
+    assert(y.str == "test");
+
+    alloc_aware z("test2", std::allocator<char>());
+    assert(z.str == "test2");
+
     assert(true);
 }
